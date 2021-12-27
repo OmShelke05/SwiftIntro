ac956207c8272c97fbb74ca71890741f2bd0179c
OmShelke05 <78234011+OmShelke05@users.noreply.github.com>
2021-12-27 10:26:20 -0800

    Add files via upload

---
+++ BrickBreaker/BrickBreaker/BrickBreaker/BrickBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrickBreakerApp.swift
+//  BrickBreaker
+//
+//  Created by OM SHELKE on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct BrickBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrickBreaker/BrickBreaker/BrickBreaker/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  BrickBreaker
+//
+//  Created by OM SHELKE on 11/19/21.
+//
+
+import SwiftUI
+import SpriteKit
+
+struct ContentView: View {
+    
+    @AppStorage("bestScore") var bestScore = 0
+    @StateObject private var gameScene = GameScene()
+    
+    var body: some View {
+        ZStack{
+            SpriteView(scene: gameScene)
+            
+            VStack(alignment: .leading) {
+                Text("Level: \(gameScene.level)")
+                    .font((.system(size: 18, weight: .heavy, design: .rounded)))
+                    .foregroundColor(.white)
+                    .padding(.leading)
+                    .padding(.top, 42)
+                
+                Text("Score: \(gameScene.score)")
+                    .font((.system(size: 24, weight: .heavy, design: .rounded)))
+                    .foregroundColor(.white)
+                    .padding(.leading)
+                
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            if gameScene.isGameOver {
+                VStack {
+                    Text("Game Over")
+                        .font((.system(size: 42, weight: .heavy, design: .rounded)))
+                        .foregroundColor(.red)
+                        .padding(.leading)
+                    
+                    if gameScene.score > bestScore {
+                        Text("New Best Score!!!")
+                            .font((.system(size: 42, weight: .heavy, design: .rounded)))
+                            .foregroundColor(.green)
+                            .padding(.leading)
+                        
+                        Text("\(gameScene.score)")
+                            .font((.system(size: 42, weight: .heavy, design: .rounded)))
+                            .foregroundColor(.white)
+                            .padding(.leading)
+                    }
+                    
+                    Text("Play Again?")
+                        .font((.system(size: 24, weight: .heavy, design: .rounded)))
+                        .foregroundColor(.white)
+                        .padding()
+                        .onTapGesture{
+                            if gameScene.score > bestScore {
+                                bestScore = gameScene.score
+                            }
+                            
+                            gameScene.isGameOver.toggle()
+                            gameScene.makeBall()
+                            gameScene.makeBricks()
+                            gameScene.score = 0
+                        }
+                }
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
